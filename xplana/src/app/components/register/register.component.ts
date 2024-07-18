658e902159abdc4fa9cbca88d266942193078363
Xhechar <115901405+Xhechar@users.noreply.github.com>
2024-07-18 14:28:20 +0300

    register form,added. With input validations

--- xplana/src/app/components/register/register.component.ts
+++ xplana/src/app/components/register/register.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  constructor() { }
+  
+  registerUser(user: any) {
+    console.log(user);
+    
+  }
 
 }
